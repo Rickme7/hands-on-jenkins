3b5071429edb5825dd39c926aa88e834f8557a83
Rickme7 <rm2_ultimatum@hotmail.com>
2022-02-13 14:57:06 -0500

    Crear un job con Jenkins

---
+++ section_2/code/job_parameter.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# variable ${name} is passed as a parameter from the Jenkins job
+echo "Informática Social - Tecnología y Métodos"
+echo "Cooperación en Proyectos Software y Gestores de Contenidos"
+echo "Práctica IC"
+echo "Nombre: ${name}"
